ad2aeecb868c45d4d240d41b5d42033211633bec
KaiYin77 <kevin880208@gmail.com>
2023-01-04 15:36:35 +0800

    update trainer-related option

--- utils/get_opts.py
+++ utils/get_opts.py
@@ -1,16 +1,19 @@
 import argparse
+import yaml
 
+def create_yaml_parser():
+    with open('config.yaml', 'r') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+        return config
 
 def get_opts():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--root_dir', type=str,
-                        default='data')
     parser.add_argument('--lr', type=float,
                         default=2e-4)
     parser.add_argument('--batch_size', type=int,
-                        default=128)
+                        default=24)
     parser.add_argument('--num_epoch', type=int,
-                        default=10)
+                        default=30)
     parser.add_argument('--num_workers', type=int,
                         default=8)
     parser.add_argument('--warmup_steps', type=int,
@@ -27,6 +30,9 @@ def get_opts():
 
     parser.add_argument('--weight', type=str,
                         default=None)
+    parser.add_argument('--save_top_k', type=int,
+                        default=3)
+    parser.add_argument('--train', action='store_true')
     parser.add_argument('--test', action='store_true')
     parser.add_argument('--exp_name', type=str,
                         default='exp')
@@ -35,14 +41,14 @@ def get_opts():
     parser.add_argument('--t_range', type=int,
                         default=1000)
     parser.add_argument('--beta_small', type=float,
-        default=1e-4)
+                        default=1e-4)
     parser.add_argument('--beta_large', type=float,
-        default=0.02)
+                        default=0.02)
     parser.add_argument('--channel', type=int,
-        default=3)
+                        default=3)
     parser.add_argument('--in_dim', type=int,
-        default=28**2)
+                        default=28**2)
     parser.add_argument('--n_gen', type=int,
-        default=1)
+                        default=1)
 
     return parser.parse_args()
