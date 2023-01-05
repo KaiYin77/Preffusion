5b646e02201c9608a533d86cb92a756dbea75800
Jerry <jerry8137@gmail.com>
2023-01-05 11:19:57 +0800

    copy vae

--- models/vae.py
+++ models/vae.py
@@ -1,12 +1,37 @@
 import torch
-from models import BaseVAE
 from torch import nn
 from torch.nn import functional as F
 
-from typing import List, TypeVar
+from typing import List, TypeVar, Any
 # from torch import tensor as Tensor
+from abc import abstractmethod
 
 Tensor = TypeVar('torch.tensor')
+
+
+class BaseVAE(nn.Module):
+    def __init__(self) -> None:
+        super(BaseVAE, self).__init__()
+
+    def encode(self, input: Tensor) -> List[Tensor]:
+        raise NotImplementedError
+
+    def decode(self, input: Tensor) -> Any:
+        raise NotImplementedError
+
+    def sample(self, batch_size: int, current_device: int, **kwargs) -> Tensor:
+        raise NotImplementedError
+
+    def generate(self, x: Tensor, **kwargs) -> Tensor:
+        raise NotImplementedError
+
+    @abstractmethod
+    def forward(self, *inputs: Tensor) -> Tensor:
+        pass
+
+    @abstractmethod
+    def loss_function(self, *inputs: Any, **kwargs) -> Tensor:
+        pass
 
 
 class VanillaVAE(BaseVAE):
