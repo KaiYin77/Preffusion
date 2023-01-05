e8a7ecae8ab3caff1077e78427a6618d85de36b8
Jerry <jerry8137@gmail.com>
2023-01-05 10:42:51 +0800

    open attention module

--- models/unet.py
+++ models/unet.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 
 from torch.nn.modules.normalization import GroupNorm
 
+import ipdb
 
 def get_norm(norm, num_channels, num_groups):
     if norm == "in":
@@ -66,6 +67,7 @@ class Downsample(nn.Module):
         self.downsample = nn.Conv2d(in_channels, in_channels, 3, stride=2, padding=1)
     
     def forward(self, x, time_emb, y):
+        # ipdb.set_trace()
         if x.shape[2] % 2 == 1:
             raise ValueError("downsampling tensor height should be even")
         if x.shape[3] % 2 == 1:
@@ -187,7 +189,7 @@ class ResidualBlock(nn.Module):
         self.class_bias = nn.Embedding(num_classes, out_channels) if num_classes is not None else None
 
         self.residual_connection = nn.Conv2d(in_channels, out_channels, 1) if in_channels != out_channels else nn.Identity()
-        #self.attention = nn.Identity() if not use_attention else AttentionBlock(out_channels, norm, num_groups)
+        self.attention = nn.Identity() if not use_attention else AttentionBlock(out_channels, norm, num_groups)
     
     def forward(self, x, time_emb=None, y=None):
         out = self.activation(self.norm_1(x))
